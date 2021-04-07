68a902334532b72f96817ed377895363484f7b54
KunWardhana <kun_wardhana@yahoo.com>
2021-04-07 14:31:16 +0700

    inisialisasi environtment

---
+++ main.js
@@ -0,0 +1,34 @@
+const products = document.getElementById('products')
+
+const urlproduct = ("https://fakestoreapi.com/products")
+
+function createNode(element) {
+    return document.createElement(element)
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+fetch(urlproduct)
+.then(response => response.json())
+.then((json) => {
+    console.log(json)
+     json.map((product) => {
+     let li = createNode("li")
+     let img = createNode("img")
+     let span = createNode("span")
+
+    img.setAttribute("width", "200")
+    //jangan lupa pake kurung kurawal bukan kurun [] kek spongebob
+     img.src=product.image;
+     span.innerHTML = `${product.title}, ${product.price}`
+
+    append(li, img)
+    append(li, span)
+    append(products, li)
+ })
+
+ 
+})
+.catch((error)=>console.log(error))
