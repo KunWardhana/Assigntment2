f1fa260cf8ae6b34d10a50a126b3b8bbcbacf412
Daniel22-binus <daniel025@binus.ac.id>
2021-04-07 15:21:08 +0700

    buat table product

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const products = document.getElementById('products')
+const products = document.getElementById('productsTable')
 
 const urlproduct = ("https://fakestoreapi.com/products")
 
@@ -15,17 +15,31 @@ fetch(urlproduct)
 .then((json) => {
     console.log(json)
      json.map((product) => {
-     let li = createNode("li")
+     let tr = createNode("tr")
+     let th1 = createNode("th");
+     let th2 = createNode("th");
+     let th3 = createNode("th");
      let img = createNode("img")
-     let span = createNode("span")
+     let spanTitle = createNode("span")
+     let spanPrice = createNode("price");
 
     img.setAttribute("width", "200")
-     img.src=product.image;
-     span.innerHTML = `${product.title}, ${product.price}`
+    img.src=product.image;
+    spanTitle.innerHTML = `${product.title}`
+    spanPrice.innerHTML = `${product.price}`
 
-    append(li, img)
-    append(li, span)
-    append(products, li)
+
+    //append colomb 1
+    append(th1, img);
+    //append colomb 2
+    append(th2, spanTitle);
+    //append colomb 3
+    append(th3, spanPrice);
+    //append all
+    append(tr, th1)
+    append(tr, th2)
+    append(tr, th3)
+    append(products, tr)
  })
 
  
